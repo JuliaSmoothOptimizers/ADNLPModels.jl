88bd1f76c4a2466ce310c844bf09d4c629628f26
Alexis Montoison <alexis.montoison@polymtl.ca>
2021-01-07 10:38:09 -0500

    ADNLPModel clean

--- src/ADNLPModels.jl
+++ src/ADNLPModels.jl
@@ -1,27 +1,27 @@
-# __precompile__()
-
 module ADNLPModels
 
 using LinearAlgebra
 
 using NLPModels
 
-# using FastClosures
 using ForwardDiff
 using ReverseDiff
+using SparsityDetection
+using SparseDiffTools
+using SparseArrays
 
 export RADNLPModel
-
-# abstract type for models with automatic differentiation
-abstract type AbstractADNLPModel <: AbstractNLPModel; end
 
 mutable struct RADNLPModel <: AbstractADNLPModel
   meta :: NLPModels.NLPModelMeta
   counters :: NLPModels.Counters
+  # Functions
   f
-  ∇f!
-  ∇²fprod!
+  c
 end
+
+show_header(io :: IO, model :: RADNLPModel) = println(io, "RADNLPModel - Model with automatic differentiation")
+
 
 
 function RADNLPModel(f, x0::AbstractVector; name::String="GenericADNLPModel")
@@ -62,33 +62,86 @@ function RADNLPModel(f, x0::AbstractVector; name::String="GenericADNLPModel")
   #   ReverseDiff.gradient!(hv, x -> dot(∇f!(g, x), v), x)
   # end
 
+  # function jacvec!(model::RADNLPModel, x::AbstractVector, v::AbstractVector, jv::AbstractVector)
+  #   z = map(ForwardDiff.Dual, x, v)  # z = x + ε * v
+  #   fz = f(z)  # f(x + εv) = f(x) + ε * J(x) * v
+  #   jv = ForwardDiff.extract_derivative!(Nothing, jv, fz)  # Jf(x) * y
+  #   return Ap
+  # end
+
   return RADNLPModel(meta, counters, f, ∇f!, ∇²fprod!)
 end
 
-
-function NLPModels.obj(model::RADNLPModel, x::AbstractVector)
-  model.counters.neval_obj += 1
+function NLPModels.obj(model :: RADNLPModel, x :: AbstractVector)
+  increment!(model, :neval_obj)
   return model.f(x)
 end
 
-
-function NLPModels.grad!(model::RADNLPModel, x::AbstractVector, g::AbstractVector)
-  model.counters.neval_grad += 1
-  return model.∇f!(g, x)
+function NLPModels.grad!(model :: RADNLPModel, x :: AbstractVector, g :: AbstractVector)
+  increment!(model, :neval_grad)
+  ...
+  return 
 end
 
-
-function NLPModels.hprod!(model::RADNLPModel, x::AbstractVector, v::AbstractVector, Hv::AbstractVector; kwargs...)
-  model.counters.neval_hprod += 1
-  return model.∇²fprod!(x, v, Hv)
+function NLPModels.cons!(model :: RADNLPModel, x :: AbstractVector, c :: AbstractVector)
+  increment!(model, :neval_cons)
+  return c
 end
 
+function NLPModels.jac_structure!(model :: RADNLPModel, rows :: AbstractVector{<: Integer}, cols :: AbstractVector{<: Integer})
+  y = similar(model.meta.x0)
+  J = jacobian_sparsity(c, y, model.meta.x0)
+  rows, cols, _ = findnz(J)
+  ...
+  return rows, cols
+end
 
-function jacvec!(model::RADNLPModel, x::AbstractVector, v::AbstractVector, jv::AbstractVector)
-  z = map(ForwardDiff.Dual, x, v)  # z = x + ε * v
-  fz = f(z)  # f(x + εv) = f(x) + ε * J(x) * v
-  jv = ForwardDiff.extract_derivative!(Nothing, jv, fz)  # Jf(x) * y
-  return Ap
+function NLPModels.jac_coord!(model :: RADNLPModel, x :: AbstractVector, vals :: AbstractVector)
+  increment!(model, :neval_jac)
+  ...
+  return vals
+end
+
+function NLPModels.jprod!(model :: RADNLPModel, x :: AbstractVector, v :: AbstractVector, Jv :: AbstractVector)
+  increment!(model, :neval_jprod)
+  ...
+  return Jv
+end
+
+function NLPModels.jtprod!(model :: RADNLPModel, x :: AbstractVector, v :: AbstractVector, Jtv :: AbstractVector)
+  increment!(model, :neval_jtprod)
+  ...
+  return Jtv
+end
+
+function NLPModels.hess_structure!(model :: RADNLPModel, rows :: AbstractVector{<: Integer}, cols :: AbstractVector{<: Integer})
+  H = hessian_sparsity(f, model.meta.x0)
+  rows, cols, _ = findnz(H)
+  return rows, cols
+end
+
+function NLPModels.hess_coord!(model :: RADNLPModel, x :: AbstractVector, y :: AbstractVector, vals :: AbstractVector; obj_weight :: Float64=1.0)
+  increment!(model, :neval_hess)
+  ...
+  return vals
+end
+
+function NLPModels.hess_coord!(model :: RADNLPModel, x :: AbstractVector, vals :: AbstractVector; obj_weight :: Float64=1.0)
+  increment!(model, :neval_hess)
+  ...
+  return vals
+end
+
+function NLPModels.hprod!(model :: RADNLPModel, x :: AbstractVector, y :: AbstractVector, v :: AbstractVector, hv :: AbstractVector; obj_weight :: Float64=1.0)
+  increment!(model, :neval_hprod)
+  ...
+  return hv
+end
+
+function NLPModels.hprod!(model :: RADNLPModel, x :: AbstractVector, v :: AbstractVector, hv :: AbstractVector; obj_weight :: Float64=1.0)
+  increment!(model, :neval_hprod)
+  ...
+  return hv
 end
 
 end
