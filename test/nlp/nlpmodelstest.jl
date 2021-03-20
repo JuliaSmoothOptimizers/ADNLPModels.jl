460ae5e88476749566a5d88b514d9dfed53be1f8
Mohamed Tarek <mohamed82008@gmail.com>
2021-03-21 08:24:22 +1100

    more tests

--- test/nlp/nlpmodelstest.jl
+++ test/nlp/nlpmodelstest.jl
@@ -1,25 +1,30 @@
-for problem in NLPModelsTest.nlp_problems
-  @testset "Checking NLPModelsTest tests on problem $problem" begin
-    nlp_ad = eval(Meta.parse(lowercase(problem) * "_autodiff"))()
-    nlp_man = eval(Meta.parse(problem))()
+@testset "AD backend - $(adbackend)" for adbackend in [
+  ForwardDiffAD(), ZygoteAD(), ReverseDiffAD(),
+]
+  for problem in NLPModelsTest.nlp_problems
+    @testset "Checking NLPModelsTest tests on problem $problem" begin
+      nlp_ad = eval(Meta.parse(lowercase(problem) * "_autodiff"))()
+      nlp_ad = switch_adbackend(nlp_ad, adbackend)
+      nlp_man = eval(Meta.parse(problem))()
 
-    show(IOBuffer(), nlp_ad)
+      show(IOBuffer(), nlp_ad)
 
-    nlps = [nlp_ad, nlp_man]
-    @testset "Check Consistency" begin
-      consistent_nlps(nlps)
-    end
-    @testset "Check dimensions" begin
-      check_nlp_dimensions(nlp_ad)
-    end
-    @testset "Check multiple precision" begin
-      multiple_precision_nlp(nlp_ad)
-    end
-    @testset "Check view subarray" begin
-      view_subarray_nlp(nlp_ad)
-    end
-    @testset "Check coordinate memory" begin
-      coord_memory_nlp(nlp_ad)
+      nlps = [nlp_ad, nlp_man]
+      @testset "Check Consistency" begin
+        consistent_nlps(nlps)
+      end
+      @testset "Check dimensions" begin
+        check_nlp_dimensions(nlp_ad)
+      end
+      @testset "Check multiple precision" begin
+        multiple_precision_nlp(nlp_ad)
+      end
+      @testset "Check view subarray" begin
+        view_subarray_nlp(nlp_ad)
+      end
+      @testset "Check coordinate memory" begin
+        coord_memory_nlp(nlp_ad)
+      end
     end
   end
 end
