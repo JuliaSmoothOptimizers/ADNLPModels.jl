f975ec266a54f25bc120b92d560929bc7cdedaa1
Monssaf Toukal <tmonssaf@gmail.com>
2021-07-04 18:28:50 -0400

    add push_benchmark bash script

---
+++ push_benchmarks.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+julia benchmark/send_comment_to_pr.jl -o $org -r $repo -p $pullrequest -c '**Starting benchmarks!**'
+
+git checkout $BRANCH_NAME --
+
+if [ "$?" -ne "0" ] ; then
+    LOCAL_BRANCH_NAME="temp_bmark"
+    git fetch origin pull/$pullrequest/head:$LOCAL_BRANCH_NAME
+    git checkout $LOCAL_BRANCH_NAME --
+fi
+
+julia benchmark/$1 $repo
+
+if [ "$?" -eq "0" ] ; then
+    julia benchmark/send_comment_to_pr.jl -o $org -r $repo -p $pullrequest -g
+else
+    julia benchmark/send_comment_to_pr.jl -o $org -r $repo -p $pullrequest -c "**An error has occured while running the benchmark script: $1** "
+fi
+
+git checkout master
+
+git branch -D $LOCAL_BRANCH_NAME
