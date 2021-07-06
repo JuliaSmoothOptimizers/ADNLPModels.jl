d81e03cd22abbd24a049631002d61d0581085ee3
tmigot <tangi.migot@gmail.com>
2021-07-06 12:53:30 -0400

    simplify benchmark

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -6,7 +6,19 @@ using NLPModels, BenchmarkProfiles, SolverBenchmark, NLPModelsJuMP
 #This package
 using ADNLPModels, ReverseDiff, Zygote, ForwardDiff
 
-include("problems.jl")
+using Pkg
+Pkg.add(url="https://github.com/tmigot/ADNLPModelProblems")
+using ADNLPModelProblems, NLPModelsJuMP
+
+# Scalable problems from ADNLPModelProblems.jl
+const problems = ["clnlbeam", "controlinvestment", "hovercraft1d", "polygon1", "polygon2", "polygon3"]
+
+nn = ADNLPModelProblems.default_nvar # 100 # default parameter for scalable problems
+# available functions:
+# $(pb)_autodiff(args... ; n=$(nn), kwargs...)
+# $(pb)_reverse(args... ; kwargs...)
+# $(pb)_zygote(args... ; kwargs...)
+# $(pb)_jump(args... ; n=$(nn), kwargs...)
 
 models = [:reverse, :zygote, :autodiff, :jump]
 fun    = Dict(:obj => (nlp, x) -> obj(nlp, x), 
@@ -28,7 +40,7 @@ for f in keys(fun)
 end
 
 for pb in problems, m in models
-  npb = eval(Meta.parse("$(pb)_$(m)()"))
+  npb = eval(Meta.parse("$(pb)_$(m)()")) # we should add a kwargs n=(size_of_problem) to modify the size
   for (fs, f) in fun
     x = npb.meta.x0
     SUITE[fs][m][string(pb)] = @benchmarkable eval($f)($npb, $x)
