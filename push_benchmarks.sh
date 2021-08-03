76fa50dd3617dfd643f6b6392ab62763cc3e6948
MonssafToukal <32516773+MonssafToukal@users.noreply.github.com>
2021-08-03 15:26:59 -0400

    comment creation before checkout

--- push_benchmarks.sh
+++ push_benchmarks.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 git checkout $BRANCH_NAME --
 
+julia benchmark/send_comment_to_pr.jl -o $org -r $repo -p $pullrequest -c '**Starting benchmarks!**'
+
 if [ "$?" -ne "0" ] ; then
     LOCAL_BRANCH_NAME="temp_bmark"
     git fetch origin pull/$pullrequest/head:$LOCAL_BRANCH_NAME
     git checkout $LOCAL_BRANCH_NAME --
 fi
-julia benchmark/send_comment_to_pr.jl -o $org -r $repo -p $pullrequest -c '**Starting benchmarks!**'
 
 julia benchmark/$1 $repo
 
