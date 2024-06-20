3cef6a267cb427768ddede055a120ed8e2ed0429
tmigot <tangi.migot@gmail.com>
2024-06-20 13:47:17 +0200

    jac benchmark

---
+++ benchmark/jacobian/benchmarks_coloring.jl
@@ -0,0 +1,50 @@
+#=
+INTRODUCTION OF THIS BENCHMARK:
+
+We test here the `jacobian_backend` for ADNLPModels with different backends:
+  - ADNLPModels.SparseADJacobian;
+  - ADNLPModels.SparseADJacobian with Symbolics for sparsity detection.
+=#
+using ForwardDiff, SparseConnectivityTracer, SparseMatrixColorings, Symbolics
+
+include("additional_backends.jl")
+
+data_types = [Float64] 
+
+benchmark_list = [:optimized]
+
+benchmarked_jac_coloring_backend = Dict(
+  "sparse" => ADNLPModels.SparseADJacobian,
+  "sparse_symbolics" => (nvar, f, ncon, c!; kwargs...) -> ADNLPModels.SparseADJacobian(nvar, f, ncon, c!; detector = SymbolicsSparsityDetector(), kwargs...),
+  # add ColPack?
+)
+get_backend_list(::Val{:optimized}) = keys(benchmarked_jac_coloring_backend)
+get_backend(::Val{:optimized}, b::String) = benchmarked_jac_coloring_backend[b]
+
+problem_sets = Dict("scalable" => scalable_cons_problems)
+nscal = 1000
+
+name_backend = "jacobian_backend"
+fun = :jacobian_backend
+@info "Initialize $(fun) benchmark"
+SUITE["$(fun)"] = BenchmarkGroup()
+
+for f in benchmark_list
+  SUITE["$(fun)"][f] = BenchmarkGroup()
+  for T in data_types
+    SUITE["$(fun)"][f][T] = BenchmarkGroup()
+    for s in keys(problem_sets)
+      SUITE["$(fun)"][f][T][s] = BenchmarkGroup()
+      for b in get_backend_list(Val(f))
+        SUITE["$(fun)"][f][T][s][b] = BenchmarkGroup()
+        backend = get_backend(Val(f), b)
+        for pb in problem_sets[s]
+          n = eval(Meta.parse("OptimizationProblems.get_" * pb * "_nvar(n = $(nscal))"))
+          m = eval(Meta.parse("OptimizationProblems.get_" * pb * "_ncon(n = $(nscal))"))
+          @info " $(pb): $T with $n vars and $m cons"
+          SUITE["$(fun)"][f][T][s][b][pb] = @benchmarkable set_adnlp($pb, $(name_backend), $backend, $nscal, $T)
+        end
+      end
+    end
+  end
+end
