f23c1ab175a871c712e1155a200eec72847b3d9f
tmigot <tangi.migot@gmail.com>
2024-07-04 13:29:08 +0200

    Add more benchmark CI

--- benchmark/benchmarks_Hessian.jl
+++ benchmark/benchmarks_Hessian.jl
@@ -13,8 +13,6 @@ verbose_subbenchmark = false
 # Run locally with `tune!(SUITE)` and then `run(SUITE)`
 const SUITE = BenchmarkGroup()
 
-include("gradient/benchmarks_gradient.jl")
-
 include("jacobian/benchmarks_coloring.jl")
 include("jacobian/benchmarks_jacobian.jl")
 include("jacobian/benchmarks_jacobian_residual.jl")
@@ -23,11 +21,3 @@ include("hessian/benchmarks_coloring.jl")
 include("hessian/benchmarks_hessian.jl")
 include("hessian/benchmarks_hessian_lagrangian.jl")
 include("hessian/benchmarks_hessian_residual.jl")
-
-include("jacobian/benchmarks_jprod.jl")
-include("jacobian/benchmarks_jprod_residual.jl")
-include("jacobian/benchmarks_jtprod.jl")
-include("jacobian/benchmarks_jtprod_residual.jl")
-
-include("hessian/benchmarks_hprod.jl")
-include("hessian/benchmarks_hprod_lagrangian.jl")
